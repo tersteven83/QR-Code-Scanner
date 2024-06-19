7c55955d87d827640a04443d2cd8471ce3be4529
steevi <testeven83@gmail.com>
2024-06-19 11:18:05 +0300

    ajout et mise à jour de étudiant

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import uvicorn
 
 from fastapi import FastAPI
 
-from .helpers import models
-from .helpers.database import engine
-from .routers import etudiant
+from helpers import models
+from helpers.database import engine
+from routers import etudiant
 
 models.Base.metadata.create_all(engine)
 
@@ -18,5 +18,5 @@ async def root():
     return {"message": "Hello Bigger Applications!"}
 
 
-# if __name__ == "__main__":
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
